d3f48ffe043b4f51910fb1cacd3964bb1042be4b
Juhyung990122 <harukawa99@naver.com>
2021-02-04 03:47:22 +0900

    Edit meta-config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `여름산책`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
+  author: `이주형`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://juhyung990122.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
